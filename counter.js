9b5790d47cd2f30bc0ad5e5d9b262039ec302b6c
Apprentice <jacoboziah@gmail.com>
2017-04-05 14:18:19 -0700

    counter thing

--- counter.js
+++ counter.js
@@ -1,3 +1,26 @@
 $(document).ready(function(){
-  // Write your code here
+
+$('#add').on('click', function(){
+$('#result').text(parseInt($('#result').text()) + 1);
+$('#result').addClass('changing')
+setTimeout(function(){$('#result').removeClass('changing')},200)
+});
+$('#subtract').on('click', function(){
+$('#result').text(parseInt($('#result').text()) - 1);
+$('#result').addClass('changing')
+setTimeout(function(){$('#result').removeClass('changing')},200)
+});
+$('.counter').clone().prop('class', 'counter_2'). appendTo('.counter')
+$('.counter_2 #add').on('click', function(){
+$('.counter_2 #result').text(parseInt($('#result2').text()) + 2);
+$('.counter_2 #result').addClass('changing')
+setTimeout(function(){$('#result').removeClass('changing')},200)
+});
+$('.counter_2 #subtract').on('click', function(){
+$('.counter_2 #result').text(parseInt($('#result').text()) - 2);
+$('.counter_2 #result').addClass('changing')
+setTimeout(function(){$('#result').removeClass('changing')},200)
+});
+
+
 })
--- traverse.js
+++ traverse.js
@@ -1,3 +1,20 @@
 $(document).ready(function(){
   // Write your code here
+  $ ('h1').text()
+  $('.last_name').text('Eich')
+  $('li').text()
+  $('#person_1').clone().prop('id', 'person_2'). appendTo('.container')
+  $('#person_2 > .first_name').text('Jacob Oziah')
+  $('#person_2 > .last_name').text(', Owen Reeder')
+  $('#person_2 li:first').text('Stuff')
+  $('#person_2 li:odd').text('Stuff stuff')
+  $('#person_2 li:last').text('Stuff stuff stuff')
+  $('#person_2').on('click','li',function(event){
+    alert("Hello! I am an alert box!!");
+  })
+  $('.container').on('click','h1',function(event){
+  $('ul').toggle()
+
+  })
+
 })
