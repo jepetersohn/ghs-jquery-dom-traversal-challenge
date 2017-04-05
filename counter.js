adc7e7eafc88ed8dcadde359e5ee9360aaf6d866
Matt <matthew.t.miner@gmail.com>
2017-04-05 16:10:28 -0700

    Implement all releases

--- counter.js
+++ counter.js
@@ -1,3 +1,27 @@
 $(document).ready(function(){
-  // Write your code here
+  $('.counter').clone().prop('class', '.counter_2'). appendTo('.counter')
+
+  $(".counter #add").on('click', function() {
+    $(".counter #result").text(parseInt($(".counter #result").text())+1);
+    $(".counter #result").addClass("changing")
+    setTimeout(function(){ $(".counter #result").removeClass("changing") }, 200)
+  });
+
+  $(".counter #subtract").on('click', function() {
+    $(".counter #result").text(parseInt($(".counter #result").text())-1);
+    $(".counter #result").addClass("changing")
+    setTimeout(function(){ $(".counter #result").removeClass("changing") }, 200)
+  });
+
+  $(".counter_2 #add").on('click', function() {
+    $(".counter_2 #result").text(parseInt($(".counter_2 #result").text())+1);
+    $(".counter_2 #result").addClass("changing")
+    setTimeout(function(){ $(".counter_2 #result").removeClass("changing") }, 200)
+  });
+
+  $(".counter_2 #subtract").on('click', function() {
+    $(".counter_2 #result").text(parseInt($(".counter_2 #result").text())-1);
+    $(".counter_2 #result").addClass("changing")
+    setTimeout(function(){ $(".counter_2 #result").removeClass("changing") }, 200)
+  });
 })
--- traverse.js
+++ traverse.js
@@ -1,3 +1,18 @@
 $(document).ready(function(){
-  // Write your code here
-})
+  $('h1').text()
+  $('li').text()
+  $('#person_1').clone().prop('id', 'person_2'). appendTo('.container')
+  $('span first_name').text("Joey")
+  $('span.first_name').text("Joey")
+  $('#person_2 span.last_name').text("Miner")
+  $('#person_1 span.last_name').text("Eich")
+  $('#person_1 span.first_name').text("Brendon")
+  $('#person_2 li:first').text("C#")
+
+  $( "#person_1" ).on('click','li',function() {
+  alert( "Something has been clicked" );
+  });
+  $( '.container' ).on('click','h1',function() {
+    $( "ul" ).toggle();
+  });
+  });
