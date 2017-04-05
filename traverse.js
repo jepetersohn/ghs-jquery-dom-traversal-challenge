01190851193b9263685786706ae8b9e42fb455ff
Chris <willisnjchris@yahoo.com>
2017-04-05 11:12:42 -0700

    person_2

--- traverse.js
+++ traverse.js
@@ -1,4 +1,7 @@
 $(document).ready(function(){
+   var person = $("#person_1").clone()
    $(".last_name").text("Eich")
-   
+   person.attr("id", "person_2")
+   person.find(".first_name").text("Erin")
+   person.find(".last_name").text("Willis")
 })
