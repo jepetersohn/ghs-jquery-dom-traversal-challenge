5357dbe494372dac398339bb9f2c5b9205c3dcb7
kanomc <kaley.hobo1@gmail.com>
2017-04-05 16:09:09 -0700

    potato

---
+++ traverse.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+  $("h1").text();
+  $(".person .last_name").text("Eich");
+  $(".person li").text();
+  $('.person').clone().insertAfter("div.container");
+
+
+  //$("#person_1").append();
+  //$('.person').clone().insertAfter("div.container");
+})
