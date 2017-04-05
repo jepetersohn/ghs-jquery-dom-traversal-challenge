c7acd3ed3d2d127dbeafcde09f81ca00601f3d61
cristinamisas <cristinamisas099@gmail.com>
2017-04-05 14:30:04 -0700

    update last name and add person

--- traverse.js
+++ traverse.js
@@ -1,3 +1,25 @@
 $(document).ready(function(){
-  // Write your code here
+$("h1").text();
+$( ".last_name" ).text("Eich");
+$("li")
+$("#person_1").append("Andrea Sorzano");
+$( "li" ).clone().appendTo( "#person_1" );
+
+$(".person li").click(function(){
+    alert("The list was clicked.");
+
+
+//did not do last one
+
+});
+
+
+
+
+
+
+
+
+
+
 })
